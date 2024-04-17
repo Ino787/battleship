beba48b403fa5d65598eb3b58006a3210d11ad5a
Ino787 <mirin2248@gmail.com>
2024-04-17 16:11:10 -0400

    setup webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.css';
+import { print } from './print.js'
+
+print();
+console.log('testing webpack');
---
+++ src/print.js
@@ -0,0 +1,9 @@
+function print() {
+    console.log('print')
+}
+
+
+export { print };
+
+
+
